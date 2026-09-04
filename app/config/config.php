8eebac1515dd988a7aa39dd4762e5edbf1c92f33
Mary dianne <marydiannedelosreyes131@gmail.com>
2026-09-04 20:23:43 +0800

    code

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['environment'] = getenv('APP_ENV') ?: 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'http:/127.0.0';
+$config['base_url'] 				= getenv('APP_URL') ?: 'http://localhost/';
 
 /*
 |--------------------------------------------------------------------------
--- app/config/database.php
+++ app/config/database.php
@@ -59,13 +59,13 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> getenv('localhost') ?: '',
+        'hostname'	=> trim(getenv('DB_HOST') ?: ''),
     'port'		=> getenv('DB_PORT') ?: '',
-    'username'	=> getenv('root') ?: '',
-    'password'	=> getenv(' ') ?: '',
-    'database'	=> getenv('none') ?: '',
-    'charset'	=> 'utf8',
-    'dbprefix'	=> ' ',
+        'username'	=> getenv('DB_USER') ?: '',
+        'password'	=> getenv('DB_PASSWORD') ?: '',
+        'database'	=> getenv('DB_NAME') ?: '',
+        'charset'	=> 'utf8mb4',
+        'dbprefix'	=> '',
     // Optional for SQLite
     'path'      => ''
 );
