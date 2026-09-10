3a11dd3560d8fe3b05e9bb16102deb942065911a
Mary dianne <marydiannedelosreyes131@gmail.com>
2026-09-10 20:44:57 +0800

    crud

--- app/config/middleware.php
+++ app/config/middleware.php
@@ -41,4 +41,12 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 | Used for adding middlewares
 |
 */
-$config['middlewares'] = [];
+require_once APP_DIR . 'middlewares/StudentMiddleware.php';
+require_once APP_DIR . 'middlewares/AuthMiddleware.php';
+require_once APP_DIR . 'middlewares/AdminMiddleware.php';
+
+$config['middlewares'] = [
+    'student' => new StudentMiddleware(),
+    'auth'    => new AuthMiddleware(),
+    'admin'   => new AdminMiddleware(),
+];
